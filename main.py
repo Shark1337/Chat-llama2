a86016f11021763a35d9e48f9fe9da7143d5ee79
Rubén Ródenas Cebrián <eldoctorbugs@gmail.com>
2023-07-19 15:37:48 +0200

    memory chat main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+import torch
+from llama import Llama
 from typing import Optional
 import fire
-from llama import Llama
 
 def main(
     ckpt_dir: str,
     tokenizer_path: str,
-    temperature: float = 0.6,
-    top_p: float = 0.9,
-    max_seq_len: int = 512,
-    max_batch_size: int = 4,
+    temperature: float = 1,
+    top_p: float = 1,
+    max_seq_len: int = 5000,
+    max_batch_size: int = 5,
     max_gen_len: Optional[int] = None,
 ):
     generator = Llama.build(
@@ -17,17 +18,22 @@ def main(
         max_seq_len=max_seq_len,
         max_batch_size=max_batch_size,
     )
+    
+    dialog_history = []
 
     while True:  # Loop forever
         # Accept user input from the terminal
-        user_input = input("Enter your prompt (or 'exit' to quit): ")
+        user_input = input("chat prompt (or 'exit' to quit): ")
 
         # If the user types 'exit', break the loop
         if user_input.lower() == 'exit':
             break
 
-        # Use user input as the content for the user role
-        dialogs = [[{"role": "user", "content": user_input}]]
+        # Append user input to the dialog history
+        dialog_history.append({"role": "user", "content": user_input})
+
+        # Use the entire dialog history as the context for the model
+        dialogs = [dialog_history]
         
         results = generator.chat_completion(
             dialogs,  
@@ -36,13 +42,15 @@ def main(
             top_p=top_p,
         )
 
-        for dialog, result in zip(dialogs, results):
-            for msg in dialog:
-                print(f"{msg['role'].capitalize()}: {msg['content']}\n")
+        for result in results:
+            print(f"User: {user_input}n")
             print(
-                f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+                f"> Assistant: {result['generation']['content']}"
             )
-            print("\n==================================\n")
+            # Append model's response to the dialog history
+            dialog_history.append(result['generation'])
+
+            print("n==================================n")
 
 if __name__ == "__main__":
     fire.Fire(main)
